75c34c95cd01362cf2ceb5822b70a2dc3c7d56d2
sinedied <noda@free.fr>
2022-04-14 11:48:09 +0200

    build: add debug mode

--- docker.sh
+++ docker.sh
@@ -8,7 +8,7 @@ if [ "$1" == "build" ]; then
   docker build -t dev .devcontainer
 elif [ "$1" == "run" ]; then
   echo "Starting container to generate framework samples..."
-  DEBUG=1 docker run --rm -v $(pwd):/repo -w /repo dev ./init_frameworks.sh $2
+  docker run  -e DEBUG=1 --rm -v $(pwd):/repo -w /repo dev ./init_frameworks.sh $2
 else
   echo "Usage: ./docker.sh <build|run> [--clean]"
 fi
