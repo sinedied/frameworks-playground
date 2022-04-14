826ffe71e2e194a93fd273abaaf3b713384ec210
sinedied <noda@free.fr>
2022-04-14 18:10:14 +0200

    build: fix docker run

--- docker.sh
+++ docker.sh
@@ -8,7 +8,7 @@ if [ "$1" == "build" ]; then
   docker build -t dev .devcontainer
 elif [ "$1" == "run" ]; then
   echo "Starting container to generate framework samples..."
-  docker run  -e DEBUG=1 --rm -v $(pwd):/repo -w /repo dev ./init_frameworks.sh $2
+  docker run -e DEBUG=$DEBUG -t --rm -v $(pwd):/repo -w /repo dev ./init_frameworks.sh $2
 else
   echo "Usage: ./docker.sh <build|run> [--clean]"
 fi
