86cb2b0cec26d06f23fc8a785e15db9c7e4a6c72
sinedied <noda@free.fr>
2022-04-14 20:53:17 +0200

    ci: improve publish script

--- publish_frameworks.sh
+++ publish_frameworks.sh
@@ -5,10 +5,14 @@ BRANCH=samples
 set -e
 sed -i '.old' -e 's/\/samples\///g' .gitignore
 rm -f .*.old
-git switch -c $BRANCH
-git add .
+cp .gitignore samples/.gitignore
+cd samples
 git config --global user.name "ci-rebot"
 git config --global user.email "cirebot.github@gmail.com"
+git init
+git remote add origin https://$GITHUB_TOKEN@github.com/sinedied/hadra-trance-festival.git
+git checkout --orphan $BRANCH
+git add .
 git commit -m "chore(publish): update samples"
 git push -u origin $BRANCH --force
 
