ac435de7915d0693a1b7bf0f5f48b28d23597bf8
sinedied <noda@free.fr>
2022-04-14 18:10:35 +0200

    feat: add more frameworks

--- init_frameworks.sh
+++ init_frameworks.sh
@@ -3,34 +3,43 @@
 # Usage: ./init_frameworks.sh [--clean]
 #######################################
 
+CI=true
+
 set -e
+if [ ! -z "$DEBUG" ]; then
+  echo "> Debug mode enabled <"
+fi
 if [ "$1" == "--clean" ]; then
   echo "Cleaning existing app & api folders..."
   rm -rf samples
 fi
 mkdir -p samples/api samples/app
 
-# gen <name> <command> [<create_dir>]
+# gen <name> <command> [<create_dir>] [<dir_to_rename>]
 gen() {
   name=$1
   cmd=$2
   mk_dir=$3
+  rename_dir=$4
   if [ -d $name ]; then
     echo "Skipping $name (already exists)"
     return
   fi
   echo "Generating $name..."
-  if [ $mk_dir ]; then
+  if [ "$mk_dir" == true ]; then
     mkdir $name
     pushd $name > /dev/null
   fi
   if [ -z "$DEBUG" ]; then
-    $cmd > /dev/null
+    eval "$cmd" > /dev/null
   else
-    $cmd
+    eval "$cmd"
   fi
-  if [ $mk_dir ]; then
+  if [ "$mk_dir" == true ]; then
     popd > /dev/null
+  fi
+  if [ ! -z "$rename_dir" ]; then
+    mv $rename_dir $name
   fi
 }
 
@@ -47,7 +56,6 @@ autoenter() {
 npx() {
   command npx -y "$@"
 }
-
 #######################################
 # API frameworks
 #######################################
@@ -70,7 +78,8 @@ echo "=== Generating App frameworks ==="
 
 gen static "echo '<!doctype html><html><body>Hello</body></html>' > index.html" true
 gen angular "npx @angular/cli@latest new angular --defaults --skip-git --skip-install --minimal"
-gen react "npx create-react-app@latest react-app && mv react-app react"
+gen angular-scully "npx @angular/cli@latest new angular-scully --defaults --skip-git --skip-install --minimal && cd angular-scully && ng add --skip-confirmation --defaults @scullyio/init@next"
+gen react "npx create-react-app@latest react-app" false react-app
 gen preact "npx preact-cli@latest create default preact"
 gen vue "npx create-vue@latest vue --default"
 gen docusaurus "npx create-docusaurus@latest docusaurus classic --skip-install"
@@ -78,4 +87,15 @@ gen nuxtjs "npx create-nuxt-app@latest nuxtjs --answers {\"name\":\"nuxt\",\"lan
 gen nextjs "npx create-next-app nextjs --use-npm"
 gen vuepress "autoenter npx -y create-vuepress-site@latest vuepress"
 gen aurelia "npx aurelia-cli@latest new aurelia --select"
-gen gatsby "npx create-gatsby -y gatsby"
+gen gatsby "npx create-gatsby@latest -y gatsby"
+gen svelte "npx degit sveltejs/template svelte"
+gen hugo "hugo new site hugo"
+gen ember "npx ember-cli@latest new ember-app --lang en --skip-git true --skip-npm true" false ember-app
+gen riot "autoenter npx -y create-riot@latest riot" true
+gen stencil "npx create-stencil@latest app stencil"
+gen elm "yes | elm init" true
+gen django "django-admin startproject djangoapp" false djangoapp
+gen polymer "npx degit PolymerLabs/polymer-3-first-element polymer"
+gen lit "npx @open-wc/create --type scaffold --scaffoldType app --typescript true --tagName lit-app --installDependencies false --features --writeToDisk true" false lit-app
+gen marko "autoenter npx -y @marko/create@latest marko"
+gen hexo "hexo init hexo"
