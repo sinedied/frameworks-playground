bca9791d157b024a7653bb5585446b634e5e9ab1
ci-rebot <cirebot.github@gmail.com>
2022-04-15 11:42:16 +0200

    ci: fix docker user

--- docker.sh
+++ docker.sh
@@ -3,12 +3,15 @@
 # Usage: ./docker.sh <build|run> [--clean]
 ###########################################################
 
+USER_NAME=$(id -u -n)
+USER_GROUP=$(id -g -n)
+
 if [ "$1" == "build" ]; then
   echo "Building container image..."
   docker build -t dev .devcontainer
 elif [ "$1" == "run" ]; then
   echo "Starting container to generate framework samples..."
-  docker run -e DEBUG=$DEBUG -t --rm -v $(pwd):/repo -w /repo dev ./create_samples.sh $2
+  docker run --user $USER_NAME:$USER_GROUP -e DEBUG=$DEBUG -t --rm -v $(pwd):/repo -w /repo dev ./create_samples.sh $2
 else
   echo "Usage: ./docker.sh <build|run> [--clean]"
 fi
