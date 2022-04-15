2def39fef232cbae6721c1c6a9b8ac408943bfd5
ci-rebot <cirebot.github@gmail.com>
2022-04-15 11:51:37 +0200

    ci: print user id

--- docker.sh
+++ docker.sh
@@ -3,15 +3,15 @@
 # Usage: ./docker.sh <build|run> [--clean]
 ###########################################################
 
-USER_NAME=$(id -u -n)
-USER_GROUP=$(id -g -n)
+USER_ID=$(id -u)
+echo "> Current user: $USER_ID <"
 
 if [ "$1" == "build" ]; then
   echo "Building container image..."
   docker build -t dev .devcontainer
 elif [ "$1" == "run" ]; then
   echo "Starting container to generate framework samples..."
-  docker run --user $USER_NAME:$USER_GROUP -e DEBUG=$DEBUG -t --rm -v $(pwd):/repo -w /repo dev ./create_samples.sh $2
+  docker run --user $USER_ID -e DEBUG=$DEBUG -t --rm -v $(pwd):/repo -w /repo dev ./create_samples.sh $2
 else
   echo "Usage: ./docker.sh <build|run> [--clean]"
 fi
