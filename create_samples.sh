e5995eeb364c3358bba90328e00c58b63aff45f9
ci-rebot <cirebot.github@gmail.com>
2022-04-20 09:55:14 +0000

    feat: add ssr samples

--- create_samples.sh
+++ create_samples.sh
@@ -13,7 +13,6 @@ if [ "$1" == "--clean" ]; then
   echo "Cleaning existing samples..."
   rm -rf samples
 fi
-mkdir -p samples/api samples/app
 
 # gen <name> <command> [<create_dir>] [<dir_to_rename>]
 gen() {
@@ -61,9 +60,10 @@ nofail() {
 npx() {
   command npx -y "$@"
 }
-#######################################
+#########################################
 # API frameworks (for Azure Functions)
-#######################################
+#########################################
+mkdir -p samples/api
 pushd samples/api > /dev/null
 echo "=== Generating API samples ==="
 
@@ -74,14 +74,15 @@ gen python "func init --worker-runtime python" true
 gen node "func init --worker-runtime node" true
 gen node-ts "func init --worker-runtime node --language typescript" true
 
-#######################################
-# App frameworks
-#######################################
+#########################################
+# App frameworks (Static Sites)
+#########################################
 popd > /dev/null
+mkdir -p samples/app
 pushd samples/app > /dev/null
 echo "=== Generating App samples ==="
 
-gen static "echo '<!doctype html><html><body>Hello</body></html>' > index.html" true
+gen static "echo '<!doctype html><html><body>Hello world</body></html>' > index.html" true
 gen gatsby "npx create-gatsby@latest -y gatsby"
 gen angular "npx @angular/cli@latest new angular --defaults --skip-git --skip-install --minimal"
 gen angular-scully "npx @angular/cli@latest new angular-scully --defaults --skip-git --skip-install --minimal && cd angular-scully && nofail \"npx @angular/cli@latest add --skip-confirmation --defaults @scullyio/init@latest\""
@@ -90,7 +91,6 @@ gen preact "npx preact-cli@latest create default preact"
 gen vue "npx create-vue@latest vue --default"
 gen docusaurus "npx create-docusaurus@latest docusaurus classic --skip-install"
 gen nuxtjs "npx create-nuxt-app@latest nuxtjs --answers '{\"name\":\"nuxt\",\"language\":\"ts\",\"pm\":\"npm\",\"ui\":\"none\",\"target\":\"static\",\"features\":[],\"linter\":[],\"test\":\"none\",\"mode\":\"universal\",\"devTools\":[]}'"
-gen nextjs "npx create-next-app@latest nextjs --use-npm"
 gen vuepress "autoenter npx -y create-vuepress-site@latest vuepress"
 gen aurelia "npx aurelia-cli@latest new aurelia --select"
 gen svelte "npx degit sveltejs/template svelte"
@@ -99,7 +99,6 @@ gen riot "autoenter npx -y create-riot@latest riot" true
 gen stencil "npx create-stencil@latest app stencil"
 gen polymer "npx degit PolymerLabs/polymer-3-first-element polymer && cd polymer && cp -f demo/index.html ./ && sed -i 's/..\/node/.\/node/g' index.html && sed -i 's/demo-element.js/demo\/demo-element.js/g' index.html"
 gen lit "npx @open-wc/create@latest --type scaffold --scaffoldType app --typescript true --tagName lit-app --installDependencies false --features --writeToDisk true" false lit-app
-gen marko "autoenter npx -y @marko/create@latest marko"
 gen hexo "npx hexo-cli@latest init hexo"
 gen ionic-angular "autoenter npx -y @ionic/cli@latest start ionic-angular blank --type angular --no-deps --no-git"
 gen ionic-react "autoenter npx -y @ionic/cli@latest start ionic-react blank --type react --no-deps --no-git"
@@ -108,6 +107,18 @@ gen capacitor "npx @capacitor/create-app@latest capacitor --name capacitor --app
 gen hugo "hugo new site hugo && cd hugo && git init && git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke && echo 'theme = \"ananke\"' >> config.toml && hugo new posts/my-first-post.md && echo 'Hello world' >> content/posts/my-first-post.md"
 gen elm "yes | elm init && curl https://raw.githubusercontent.com/elm/elm-lang.org/master/examples/buttons.elm -o src/Main.elm" true
 gen django "django-admin startproject djangoapp" false djangoapp
-gen meteor "meteor create --blaze meteor --allow-superuser"
 gen blazor "dotnet new blazorserver -o blazor --no-https"
 gen flutter "flutter create flutterapp" false flutterapp
+
+#########################################
+# App frameworks (Server Side Rendering)
+#########################################
+popd > /dev/null
+mkdir -p samples/ssr
+pushd samples/ssr > /dev/null
+echo "=== Generating SSR samples ==="
+
+gen nextjs "npx create-next-app@latest nextjs --use-npm"
+gen marko "autoenter npx -y @marko/create@latest marko"
+gen meteor "meteor create --blaze meteor --allow-superuser" # build: meteor build --directory dist
+
