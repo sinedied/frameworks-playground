20cae8c66a916ea12f9839fa4d6d4af442afa3dc
sinedied <noda@free.fr>
2022-04-21 11:08:44 +0200

    feat: add docker shell command

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###########################################################
-# Usage: ./docker.sh <build|run> [--clean]
+# Usage: ./docker.sh <build|run|shell> [--clean]
 ###########################################################
 
 if [ "$1" == "build" ]; then
@@ -9,6 +9,9 @@ if [ "$1" == "build" ]; then
 elif [ "$1" == "run" ]; then
   echo "Starting container to generate framework samples..."
   docker run -e DEBUG=$DEBUG -t --rm -v $(pwd):/repo -w /repo dev ./create_samples.sh $2
+elif [ "$1" == "shell" ]; then
+  echo "Starting container with interactive shell..."
+  docker run -e DEBUG=$DEBUG -it --rm -v $(pwd):/repo -w /repo dev bash
 else
-  echo "Usage: ./docker.sh <build|run> [--clean]"
+  echo "Usage: ./docker.sh <build|run|shell> [--clean]"
 fi
